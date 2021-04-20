97245751ec5953027625ef573ae7a8bb4a530d6c
rmc3408 <rmc3408@protonmail.com>
2021-04-20 18:14:28 -0400

    deleted

--- mohammed-khan/src/services/actions/actiontypes.js
+++
@@ -1,2 +0,0 @@
-export const addCart = "ADD_CART";
-export const remCart = "REMOVE_CART";
--- src/actiontypes.js
+++
@@ -1,3 +0,0 @@
-export const ADDED = 'ADD_BUG';
-export const REMOVED = 'REMOVE_BUG';
-export const RESOLVED = 'RESOLVE_BUG';
